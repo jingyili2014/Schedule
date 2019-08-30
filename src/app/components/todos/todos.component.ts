0f034c1cc6d62bf8a2940ff2976445fcfb39047b
jingyili2014 <jingyili2014@gmail.com>
2019-08-30 13:42:42 -0400

    Schedule (todoList)
    Angular framework

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+import { TodoService } from 'src/app/services/todo.service';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor(private todoService: TodoService) {
+  }
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+}
